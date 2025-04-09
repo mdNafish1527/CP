937f96865d2ac5ddd59cc53ceb9ba53b540899e1
mdNafish1527 <166948117+mdNafish1527@users.noreply.github.com>
2025-04-10 03:13:13 +0600

    Create Ideal_Generator_Codeforces.cpp
    i don't understand but solved

---
+++ Ideal_Generator_Codeforces.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    while(t--)
+        {
+            int n;
+            cin >> n;
+            if(n % 2)
+                {
+                    cout << "YES" << endl;
+                }
+            else
+                {
+                    cout << "NO" << endl;
+                }
+        }
+    return 0;
+}
